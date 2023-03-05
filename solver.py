601e17e1c36c7eea08cfc6d50acd54d2ab2b414b
mazbrew <peteryeoh50@gmail.com>
2023-03-05 22:36:51 +0800

    added the character counter

--- solver.py
+++ solver.py
@@ -4,12 +4,16 @@ def fileReader():
     words = np.loadtxt("Library.txt", dtype= str)
     char_counts = np.zeros(26, dtype= int)
 
-    #display the total number of words for debugging
+    #display the total number of words
     print(len(words))
 
+    #iterate through the entire file to count the occurences of all letters
     for i in range(len(words)):
         for j in range(5):
-            
+            char_counts[ord(words[i][j])-97] += 1
+
+    #display the character counts
+    print(char_counts)
 
     return words
 
